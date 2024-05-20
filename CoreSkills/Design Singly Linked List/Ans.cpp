0c152ccf2c37df9e82058dff7e2ffa51ebfb0671
jerry8644 <jerry8644@gmail.com>
2024-05-20 18:32:27 +0800

    Update Ans.cpp

--- CoreSkills/Design Singly Linked List/Ans.cpp
+++ CoreSkills/Design Singly Linked List/Ans.cpp
@@ -1,15 +1,14 @@
 class Node {
    public:
     int num;
-    Node* next;
-    Node(int num, Node* node) : num(num), next(node) {
-    }
+    Node *next;
+    Node(int num, Node *node) : num(num), next(node) {}
 };
 
 class LinkedList {
    public:
-    Node* head;
-    Node* tail;
+    Node *head;
+    Node *tail;
     int length = 0;
 
     LinkedList() {
@@ -18,7 +17,7 @@ class LinkedList {
     }
 
     int get(int index) {
-        Node* target;
+        Node *target;
         if (index + 1 > length) {
             return -1;
         } else if (head == tail) {
@@ -37,7 +36,7 @@ class LinkedList {
     }
 
     void insertHead(int val) {
-        Node* node = new Node(val, nullptr);
+        Node *node = new Node(val, nullptr);
         if (head == tail) {
             head->next = node;
             tail = node;
@@ -49,15 +48,15 @@ class LinkedList {
     }
 
     void insertTail(int val) {
-        Node* node = new Node(val, nullptr);
+        Node *node = new Node(val, nullptr);
         tail->next = node;
         tail = node;
         length++;
     }
 
     bool remove(int index) {
-        Node* target;
-        Node* previous;
+        Node *target;
+        Node *previous;
         if (index + 1 > length) {
             return false;
         } else if (head == tail) {
@@ -85,7 +84,7 @@ class LinkedList {
 
     vector<int> getValues() {
         vector<int> arr;
-        Node* target = head->next;
+        Node *target = head->next;
         if (head == tail) {
             return arr;
         } else {
